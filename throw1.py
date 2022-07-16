99cd96e864d11ac2ba3a2471d8d2faabaa18c44a
beyzauzunn <99256891+beyzauzunn@users.noreply.github.com>
2022-07-16 17:01:04 +0300

    Update throw1.py

--- throw1.py
+++ throw1.py
@@ -1,6 +1,7 @@
 from dronekit import connect, VehicleMode,LocationGlobalRelative
 import time
 from pymavlink import mavutil
+
 iha = connect("/dev/serial0", baud=57600, wait_ready=False)
 
 while iha.is_armable is not True:
@@ -8,21 +9,25 @@ while iha.is_armable is not True:
     time.sleep(1)
 print("IHA arm edilebilir.")
 
-iha.mode = VehicleMode("THROW")
 iha.parameters['THROW_TYPE']=0
 iha.parameters['THROW_MOT_START']=0
 iha.parameters['THROW_NEXTMODE']=4
-time.sleep(5)
+time.sleep(3)
+
+iha.mode = VehicleMode("THROW")
+time.sleep(3)
+
 if iha.mode.name is "THROW":
     print("Throw moduna geçiş yapiliyor.")
-
-print("Throw moduna geçiş yapildi.")
+    print("Throw moduna geçiş yapildi.")
 
 iha.armed = True
 
 while iha.armed is False:
     print("IHA arm ediliyor...")
-    time.sleep(1)
+    iha.armed = True
+    time.sleep(3)
+    
 if iha.armed is True:
     print("IHA arm edildi.")
 
@@ -31,17 +36,17 @@ while iha.mode.name is not "GUIDED":
     
 if iha.mode.name is "GUIDED":
    
-    konum1=LocationGlobalRelative(41.1013957,29.0258011,3)
+    konum1=LocationGlobalRelative(41.1013957,29.0258011,5)
    
-    konum2=LocationGlobalRelative(41.1016221,29.0262571,3)
+    konum2=LocationGlobalRelative(41.1016221,29.0262571,5)
    
-    konum3=LocationGlobalRelative(41.1018626,29.0255812,3)
+    konum3=LocationGlobalRelative(41.1018626,29.0255812,5)
     
     iha.simple_goto(konum1)
-    time.sleep(10)
+    time.sleep(15)
     iha.simple_goto(konum2)
-    time.sleep(10)
+    time.sleep(15)
     iha.simple_goto(konum3)
-    time.sleep(10)
+    time.sleep(15)
     
 iha.mode=VehicleMode("LAND")
