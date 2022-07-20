067eebd5355fbabb469493cfa1bf860677a8f865
beyzauzunn <99256891+beyzauzunn@users.noreply.github.com>
2022-07-20 14:35:26 +0300

    Create rtl.py

---
+++ rtl.py
@@ -0,0 +1,25 @@
+from dronekit import connect,VehicleMode
+import time
+iha = connect('127.0.0.1:14550',wait_ready=True)
+while iha.is_armable != True
+  print("iha arm edilebilir değil")
+  time.sleep(1)
+print("iha arm edilebilir")
+
+iha.mode=VehicleMode("GUIDED")
+
+iha.armed=True
+
+while iha.armed!=True
+  print("iha arm ediliyor")
+  time.sleep(1)
+
+  print("iha arm edildi")
+ 
+iha.simple_takeoff(5)
+time.sleep(15)
+iha.simple_goto(,,)
+iha.parameters
+
+iha.parameters['LAND_SPEED']=20  
+iha.mode=VehicleMode("RTL")
